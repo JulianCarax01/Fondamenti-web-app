46431c0e8770c80eb9448f2c08caf797c83ea10c
JulianCarax01 <alexandros0117@gmail.com>
2023-06-08 10:55:20 +0200

    Fixing directory layout

---
+++ backend/components/login/loginFunction.js
@@ -0,0 +1,19 @@
+const User = require("../../models/users");
+loginUser=async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body._id, password: req.body.password });
+
+        if (!user) {
+            return res.redirect('/?error=credenzialierrate');
+        }
+
+        req.session.userId = user._id;
+        req.session.password = user.password;
+        res.send('Accesso riuscito');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Errore del server');
+    }
+}
+
+module.exports=loginUser
--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,20 +1,6 @@
-const User = require('../models/users');
+const loginFunction = require("../components/login/loginFunction")
 //Esportiamo la funzione asincrona LoginUser
 module.exports = {
-    loginUser: async (req, res) => {
-        try {
-            const user = await User.findOne({ username: req.body._id, password: req.body.password });
+    loginUser: loginFunction
+}
 
-            if (!user) {
-                return res.redirect('/?error=credenzialierrate');
-            }
-
-            req.session.userId = user._id;
-            req.session.password = user.password;
-            res.send('Accesso riuscito');
-        } catch (err) {
-            console.error(err);
-            res.status(500).send('Errore del server');
-        }
-    }
-};
