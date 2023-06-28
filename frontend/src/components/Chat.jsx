c25ab19068f3a022d13aee9fdf52093e6bb2fec0
JulianCarax01 <alexandros0117@gmail.com>
2023-06-28 16:18:09 +0200

    ygyf

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,10 +1,23 @@
 import TextContainer from "./TextContainer";
 import SendMessage from "./SendMessage";
 import '../style/style_chat.css'
+import {useEffect, useRef} from "react";
 
 export default function Chat({rightChat, otherUser, loggedUser}) {
+
+    const ref = useRef(null)
+
+    const scroll = () => {
+        const lastChild = ref.current?.lastElementChild;
+        lastChild?.scrollIntoView()
+    }
+
+    useEffect(()=>{
+        scroll()
+    })
+
     //creiamo l'interfaccia generale della chat
-    return (<div className={"boxchat"}>
+    return (<div className={"boxchat"} ref={ref}>
         <h1>{otherUser}</h1>
         <TextContainer rightChat={rightChat} loggedUser={loggedUser}></TextContainer>
         <SendMessage receiver={otherUser} sender={loggedUser} rightChat={rightChat}></SendMessage>
